505c1117c0ec475230c096659e6799051bb2aeb6
Siddhant Meena <siddhantmeena5@gmail.com>
2025-02-09 04:51:14 +0000

    snake_game: Initialize `Classes` & `Libraries` for snake_game
    1) GameBoard
    2) Snake
    3) Food

---
+++ snake_game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+using namespace std;
+
+const int BOARD_SIZE = 10;
+
+class GameBoard {}
+class Snake {}
+class Food {}
